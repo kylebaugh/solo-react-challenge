3b12ad9f9d4a5986728444a79ff3f563256cce43
Kyle Baugh <kylebaugh1@gmail.com>
2022-07-06 23:06:57 -0600

    working api call and dynamic politician component build

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ function App() {
   }
 
   const handleSearch = (e) => {
-    if(searchBy === 'rep'){
-      console.log('rep')
+    if(searchBy === 'Representative'){
       axios.get(`http://localhost:3456/representatives/${searchState}`)
         .then(res => {
           console.log('rep front end hit')
@@ -30,8 +29,7 @@ function App() {
         .catch(err => {
           console.log(err)
         })
-      }else if(searchBy === 'sen'){
-        console.log('sen')
+      }else if(searchBy === 'Senator'){
         axios.get(`http://localhost:3456/senators/${searchState}`)
           .then(res => {
             console.log('rep front end hit')
@@ -46,98 +44,103 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Search Representatives</h2>
+      <h2>Who's My Representative?</h2>
       
-      <section>
-        <p>Seach By:</p>
-        
-        <label>
-          <select value={searchBy} onChange={handleChange}>
-            <option value='Choose one...'>Choose one...</option>
-            <option value='rep'>Representative</option>
-            <option value='sen'>Senator</option>
-          </select>
-        </label>
+      <div id='dropDownSection'>
 
-        <p>You chose {searchBy}</p>
+        <section id='repSelect' className='dropDown'>
+          Representative or Senator?
+          <label>
+            <select value={searchBy} onChange={handleChange}>
+              <option value='Choose one...'>Choose one...</option>
+              <option value='Representative'>Representative</option>
+              <option value='Senator'>Senator</option>
+            </select>
+          </label>
+        </section>
+
+        <section id='stateSelect' className='dropDown'>
+          Which state?
+
+          <label>
+            <select value={searchState} onChange={handleSearchState}>
+              <option value="AL">Choose one...</option>
+              <option value="AL">Alabama</option>
+              <option value="AK">Alaska</option>
+              <option value="AZ">Arizona</option>
+              <option value="AR">Arkansas</option>
+              <option value="CA">California</option>
+              <option value="CO">Colorado</option>
+              <option value="CT">Connecticut</option>
+              <option value="DE">Delaware</option>
+              <option value="DC">District of Columbia</option>
+              <option value="FL">Florida</option>
+              <option value="GA">Georgia</option>
+              <option value="HI">Hawaii</option>
+              <option value="ID">Idaho</option>
+              <option value="IL">Illinois</option>
+              <option value="IN">Indiana</option>
+              <option value="IA">Iowa</option>
+              <option value="KS">Kansas</option>
+              <option value="KY">Kentucky</option>
+              <option value="LA">Louisiana</option>
+              <option value="ME">Maine</option>
+              <option value="MD">Maryland</option>
+              <option value="MA">Massachusetts</option>
+              <option value="MI">Michigan</option>
+              <option value="MN">Minnesota</option>
+              <option value="MS">Mississippi</option>
+              <option value="MO">Missouri</option>
+              <option value="MT">Montana</option>
+              <option value="NE">Nebraska</option>
+              <option value="NV">Nevada</option>
+              <option value="NH">New Hampshire</option>
+              <option value="NJ">New Jersey</option>
+              <option value="NM">New Mexico</option>
+              <option value="NY">New York</option>
+              <option value="NC">North Carolina</option>
+              <option value="ND">North Dakota</option>
+              <option value="OH">Ohio</option>
+              <option value="OK">Oklahoma</option>
+              <option value="OR">Oregon</option>
+              <option value="PA">Pennsylvania</option>
+              <option value="RI">Rhode Island</option>
+              <option value="SC">South Carolina</option>
+              <option value="SD">South Dakota</option>
+              <option value="TN">Tennessee</option>
+              <option value="TX">Texas</option>
+              <option value="UT">Utah</option>
+              <option value="VT">Vermont</option>
+              <option value="VA">Virginia</option>
+              <option value="WA">Washington</option>
+              <option value="WV">West Virginia</option>
+              <option value="WI">Wisconsin</option>
+              <option value="WY">Wyoming</option>
+            </select>
+          </label>
+        </section>
+
+        <button onClick={handleSearch}>Find My Reps!</button>
+      </div>
+
+    <div id='fullResults'>
+      <section id='resultHeaders'>
+          <h3>Representatives</h3>
+          <h3>Info</h3>
       </section>
 
-      <section>
-        <p>Which state?</p>
-
-        <label>
-          <select value={searchState} onChange={handleSearchState}>
-            <option value="AL">Choose one...</option>
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
-          </select>
-        </label>
-      </section>
-
-    <button onClick={handleSearch}>Search Time</button>
-
-
-    <section>
-      {repArray.map((rep) => {
-        let id = rep.name
-        return (
-          <Politician 
-            key={id}
-            item={rep}
-          />
-        )
-      })}
-
-    </section>
+          <section id='results'>
+            {repArray.map((rep) => {
+              let id = rep.name
+              return (
+                <Politician 
+                key={id}
+                item={rep}
+                />
+                )
+              })}
+          </section>
+        </div>
 
     </div>
   );
--- src/components/Politician.js
+++ src/components/Politician.js
@@ -2,11 +2,33 @@ import { useState } from "react";
 
 const Politician = (props) => {
 
+    const [showDiv, setShowDiv] = useState(false)
+
+    const showDetails = async () => {
+        setShowDiv(!showDiv)
+        console.log('server show details hit' + props.item.name)
+        await props.setPoliticianName(props.item.name)
+        console.log(props.politicianName)
+        await props.getPoliticianDetails()
+    }
+
     return (
         <div id='politicianBox'>
-            <h4>{props.item.name}</h4>
-            <h4>{props.item.party}</h4>
-            <h4>{props.item.state}</h4>
+            <div id='polBasics'>
+                <h4 className={props.item.party} onClick={()=> showDetails()}>Name: {props.item.name}</h4>
+                <h4>Party: {props.item.party}</h4>
+            </div>
+            {
+            showDiv && 
+            <div id='polDetails'>
+                <h4>First Name: {props.item.state}</h4>
+                <h4>Last Name: {props.item.district}</h4>
+                <h4>District: {props.item.phone}</h4>
+                <h4>Phone: {props.item.office}</h4>
+                <h4>Website: <a>{props.item.link}</a></h4>
+                <h4>Office: {props.item.link}</h4>
+                </div>
+            }
         </div>
     )
 }
